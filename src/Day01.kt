134591d58f3c30e02d05a00d56e47b15102fbcbc
Roland Greim <github@rolandgreim.de>
2024-12-01 11:04:05 +0100

    :star: solved day1 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,26 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input
+            .map { it.split(' ', limit = 2) }
+            .map {
+                it.first().trim().toInt() to it.last().trim().toInt()
+            }
+            .let {
+                val (left, right) = it.unzip()
+                left.sorted().zip(right.sorted())
+            }
+            .map { (it.first - it.second) }
+            .sumOf { it.absoluteValue }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    require(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
