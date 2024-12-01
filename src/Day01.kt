7b58ef16a698a69006a08223ecfe0b67615a2841
Roland Greim <github@rolandgreim.de>
2024-12-01 11:12:49 +0100

    :star: solved day1 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -14,13 +14,23 @@ fun main() {
             .map { (it.first - it.second) }
             .sumOf { it.absoluteValue }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input
+            .map { it.split(' ', limit = 2) }
+            .map {
+                it.first().trim().toInt() to it.last().trim().toInt()
+            }
+            .let { inputList ->
+                val (left, right) = inputList.unzip()
+                val counts = right.groupingBy { it }.eachCount()
+                left.map { it * counts.getOrDefault(it, 0) }
+            }
+            .sum()
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     require(part1(testInput) == 11)
+    require(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
